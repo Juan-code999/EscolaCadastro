aa2d1478d31f60e5571b60f16455b8a4fe67b916
João Gabriel <163910348+Joaonzin@users.noreply.github.com>
2024-10-07 14:54:55 -0300

    pinto

---
+++ Biblioteca/02-Repositorios/Data/InicializadorBd.cs
@@ -0,0 +1,43 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escola._02_Repositorios.Data
+{
+    public static class InicializadorBd
+    {
+        public static void Inicializar()
+        {
+            using var connection = new SQLiteConnection("Data Source=Escola.db");
+
+            string commandoSQL = @"   
+                 CREATE TABLE IF NOT EXISTS Aluno(
+                 Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                 Nome TEXT NOT NULL,
+                 Idade INTEGER NOT NULL,
+                 Peso REAL NOT NULL,
+                 Altura REAL NOT NULL
+                );";
+
+          
+            commandoSQL += @"   
+                 CREATE TABLE IF NOT EXISTS Atividades(
+                 Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                 Nome TEXT NOT NULL,
+                 Tipo TEXT NOT NULL,
+                 DataEntrega TEXT NOT NULL,
+                 NotaMaxima REAL NOT NULL,
+                 NotaObtida REAL,
+                 AlunoId INTEGER,
+                 FOREIGN KEY (AlunoId) REFERENCES Alunos(Id)
+                );";
+
+            connection.Execute(commandoSQL);
+        }
+    }
+
+}
