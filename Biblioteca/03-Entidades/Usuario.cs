8db3ba6a9183927b0c1a28a12223bee241201096
Juan <juancharlesasimoes08@gmail.com>
2024-11-11 16:32:54 -0300

    dsasd

--- Biblioteca/03-Entidades/Usuario.cs
+++ Biblioteca/03-Entidades/Usuario.cs
@@ -8,7 +8,7 @@ namespace Biblioteca._03_Entidades
 {
     public class Usuario
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
 
         public string Nome { get; set; }
 
