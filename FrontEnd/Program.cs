5970440a7ee5584ca39290b0a5785b20beff0d63
Juan <juancharlesasimoes08@gmail.com>
2024-12-11 14:16:32 -0300

    front end

--- FrontEnd/Program.cs
+++ FrontEnd/Program.cs
@@ -1,8 +1,1 @@
 ﻿using FrontEnd;
-
-HttpClient cliente = new HttpClient
-{
-    BaseAddress = new Uri("https://localhost:7068/")
-};
-Sistema sistema = new Sistema(cliente);
-sistema.IniciarSistema();
--- FrontEnd/Sistema.cs
+++ FrontEnd/Sistema.cs
@@ -1,170 +1,275 @@
-﻿using Biblioteca._03_Entidades;
-using FrontEnd.UseCases;
+﻿using System;
+using System.Collections.Generic;
 
-namespace FrontEnd;
-public class SistemaEscolar
+namespace SistemaEscolar
 {
-    private static Usuario UsuarioLogado { get; set; }
-    private readonly UsuarioUC _usuarioUC;
-    private readonly AlunoUC _alunoUC;
-    private readonly ProfessorUC _professorUC;
-    private readonly TurmaUC _turmaUC;
-    private object _UsuarioUC;
-
-    public SistemaEscolar(HttpClient cliente)
+    class Program
     {
-        _usuarioUC = new UsuarioUC(cliente);
-        _alunoUC = new AlunoUC(cliente);
-        _professorUC = new ProfessorUC(cliente);
-        _turmaUC = new TurmaUC(cliente);
+        static void Main(string[] args)
+        {
+            SistemaEscolar sistema = new SistemaEscolar();
+            sistema.IniciarSistema();
+        }
     }
 
-    public void IniciarSistema()
+    public class SistemaEscolar
     {
-        int resposta = -1;
-        while (resposta != 0)
-        {
-            if (UsuarioLogado == null)
-            {
-                resposta = ExibirLogin();
+        private static Usuario UsuarioLogado { get; set; }
+        private readonly List<Usuario> _usuarios = new List<Usuario>();
+        private readonly List<Aluno> _alunos = new List<Aluno>();
+        private readonly List<Professor> _professores = new List<Professor>();
+        private readonly List<Turma> _turmas = new List<Turma>();
 
-                if (resposta == 1)
+        public void IniciarSistema()
+        {
+            ExibirTelaInicial();
+
+            int resposta = -1;
+            while (resposta != 0)
+            {
+                if (UsuarioLogado == null)
                 {
-                    FazerLogin();
-                }
-                else if (resposta == 2)
-                {
-                    Usuario usuario = CriarUsuario();
-                    _usuarioUC.CadastrarUsuario(usuario);
-                    Console.WriteLine("Usuário cadastrado com sucesso");
-                }
-                else if (resposta == 3)
-                {
-                    List<Usuario> usuarios = _UsuarioUC.ListarUsuarios();
-                    foreach (Usuario u in usuarios)
+                    resposta = ExibirLogin();
+
+                    if (resposta == 1)
                     {
-                        Console.WriteLine(u.ToString());
+                        FazerLogin();
+                    }
+                    else if (resposta == 2)
+                    {
+                        Usuario usuario = CriarUsuario();
+                        CadastrarUsuario(usuario);
+                        Console.WriteLine("Usuário cadastrado com sucesso");
+                    }
+                    else if (resposta == 3)
+                    {
+                        ListarUsuarios();
                     }
                 }
+                else
+                {
+                    ExibirMenuPrincipal();
+                }
+            }
+        }
+
+        public void ExibirTelaInicial()
+        {
+            Console.WriteLine("Bem-vindo ao Sistema Escolar!");
+            Console.WriteLine("Por favor, revise as informações abaixo antes de continuar.");
+            Console.WriteLine("1. Verifique seus dados pessoais.");
+            Console.WriteLine("2. Confirme seu endereço.");
+            Console.WriteLine("3. Revise suas preferências de curso.");
+            Console.WriteLine("Pressione qualquer tecla para continuar...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        public int ExibirLogin()
+        {
+            Console.WriteLine("--------- LOGIN ---------");
+            Console.WriteLine("1 - Deseja Fazer Login");
+            Console.WriteLine("2 - Deseja se Cadastrar");
+            Console.WriteLine("3 - Listar Usuários Cadastrados");
+            return int.Parse(Console.ReadLine());
+        }
+
+        public Usuario CriarUsuario()
+        {
+            Usuario usuario = new Usuario();
+            Console.WriteLine("Digite seu nome: ");
+            usuario.Nome = Console.ReadLine();
+            Console.WriteLine("Digite seu username: ");
+            usuario.Username = Console.ReadLine();
+            Console.WriteLine("Digite sua senha: ");
+            usuario.Senha = Console.ReadLine();
+            Console.WriteLine("Digite seu email: ");
+            usuario.Email = Console.ReadLine();
+            return usuario;
+        }
+
+        public void CadastrarUsuario(Usuario usuario)
+        {
+            _usuarios.Add(usuario);
+        }
+
+        public void ListarUsuarios()
+        {
+            if (_usuarios.Count == 0)
+            {
+                Console.WriteLine("Nenhum usuário cadastrado.");
+                return;
+            }
+
+            foreach (Usuario usuario in _usuarios)
+            {
+                Console.WriteLine(usuario.ToString());
+            }
+        }
+
+        public void FazerLogin()
+        {
+            Console.WriteLine("Digite seu username: ");
+            string username = Console.ReadLine();
+            Console.WriteLine("Digite sua senha: ");
+            string senha = Console.ReadLine();
+
+            Usuario usuario = _usuarios.Find(u => u.Username == username && u.Senha == senha);
+
+            if (usuario == null)
+            {
+                Console.WriteLine("Usuário ou senha inválidos!!!");
             }
             else
             {
-                ExibirMenuPrincipal();
+                UsuarioLogado = usuario;
+                Console.WriteLine("Login realizado com sucesso!");
             }
         }
-    }
 
-    public int ExibirLogin()
-    {
-        Console.WriteLine("--------- LOGIN ---------");
-        Console.WriteLine("1 - Deseja Fazer Login");
-        Console.WriteLine("2 - Deseja se Cadastrar");
-        Console.WriteLine("3 - Listar Usuários Cadastrados");
-        return int.Parse(Console.ReadLine());
-    }
-
-    public Usuario CriarUsuario()
-    {
-        Usuario usuario = new Usuario();
-        Console.WriteLine("Digite seu nome: ");
-        usuario.Nome = Console.ReadLine();
-        Console.WriteLine("Digite seu username: ");
-        usuario.Username = Console.ReadLine();
-        Console.WriteLine("Digite sua senha: ");
-        usuario.Senha = Console.ReadLine();
-        Console.WriteLine("Digite seu email: ");
-        usuario.Email = Console.ReadLine();
-        return usuario;
-    }
-
-    public Aluno CriarAluno()
-    {
-        Aluno aluno = new Aluno();
-        Console.WriteLine("Digite o nome do aluno: ");
-        aluno.Nome = Console.ReadLine();
-        Console.WriteLine("Digite a data de nascimento do aluno (dd/mm/yyyy): ");
-        aluno.DataNascimento = DateTime.Parse(Console.ReadLine());
-        return aluno;
-    }
-
-    public Professor CriarProfessor()
-    {
-        Professor professor = new Professor();
-        Console.WriteLine("Digite o nome do professor: ");
-        professor.Nome = Console.ReadLine();
-        Console.WriteLine("Digite a especialidade do professor: ");
-        professor.Especialidade = Console.ReadLine();
-        return professor;
-    }
-
-    public Turma CriarTurma()
-    {
-        Turma turma = new Turma();
-        Console.WriteLine("Digite o nome da turma: ");
-        turma.Nome = Console.ReadLine();
-        Console.WriteLine("Digite a data de início da turma (dd/mm/yyyy): ");
-        turma.DataInicio = DateTime.Parse(Console.ReadLine());
-        return turma;
-    }
-
-    public void FazerLogin()
-    {
-        Console.WriteLine("Digite seu username: ");
-        string username = Console.ReadLine();
-        Console.WriteLine("Digite sua senha: ");
-        string senha = Console.ReadLine();
-        CreateUsuarioDTO usuDTO = new CreateUsuarioDTO
+        public void ExibirMenuPrincipal()
         {
-            Username = username,
-            Senha = senha
-        };
-        Usuario usuario = _usuarioUC.FazerLogin(usuDTO);
-        if (usuario == null)
-        {
-            Console.WriteLine("Usuário ou senha inválidos!!!");
+            Console.WriteLine("--------- MENU PRINCIPAL ---------");
+            Console.WriteLine("1 - Listar Alunos");
+            Console.WriteLine("2 - Cadastrar Aluno");
+            Console.WriteLine("3 - Cadastrar Professor");
+            Console.WriteLine("4 - Cadastrar Turma");
+            Console.WriteLine("Qual operação deseja realizar?");
+            int resposta = int.Parse(Console.ReadLine());
+
+            if (resposta == 1)
+            {
+                ListarAlunos();
+            }
+            else if (resposta == 2)
+            {
+                Aluno aluno = CriarAluno();
+                CadastrarAluno(aluno);
+                Console.WriteLine("Aluno cadastrado com sucesso");
+            }
+            else if (resposta == 3)
+            {
+                Professor professor = CriarProfessor();
+                CadastrarProfessor(professor);
+                Console.WriteLine("Professor cadastrado com sucesso");
+            }
+            else if (resposta == 4)
+            {
+                Turma turma = CriarTurma();
+                CadastrarTurma(turma);
+                Console.WriteLine("Turma cadastrada com sucesso");
+            }
         }
-        UsuarioLogado = usuario;
-    }
 
-    public void ExibirMenuPrincipal()
-    {
-        Console.WriteLine("1 - Listar Alunos");
-        Console.WriteLine("2 - Cadastrar Aluno");
-        Console.WriteLine("3 - Cadastrar Professor");
-        Console.WriteLine("4 - Cadastrar Turma");
-        Console.WriteLine("Qual operação deseja realizar?");
-        int resposta = int.Parse(Console.ReadLine());
+        public void ListarAlunos()
+        {
+            if (_alunos.Count == 0)
+            {
+                Console.WriteLine("Nenhum aluno cadastrado.");
+                return;
+            }
 
-        if (resposta == 1)
-        {
-            ListarAlunos();
+            foreach (Aluno aluno in _alunos)
+            {
+                Console.WriteLine(aluno.ToString());
+            }
         }
-        else if (resposta == 2)
+
+        public Aluno CriarAluno()
         {
-            Aluno aluno = CriarAluno();
-            _alunoUC.CadastrarAluno(Aluno);
-            Console.WriteLine("Aluno cadastrado com sucesso");
+            Aluno aluno = new Aluno();
+            Console.WriteLine("Digite o nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite a data de nascimento do aluno (dd/mm/yyyy): ");
+            aluno.DataNascimento = DateTime.Parse(Console.ReadLine());
+            return aluno;
         }
-        else if (resposta == 3)
+
+        public void CadastrarAluno(Aluno aluno)
         {
-            Professor professor = CriarProfessor();
-            _professorUC.CadastrarProfessor(professor);
-            Console.WriteLine("Professor cadastrado com sucesso");
+            _alunos.Add(aluno);
         }
-        else if (resposta == 4)
+
+        public Professor CriarProfessor()
         {
-            Turma turma = CriarTurma();
-            _turmaUC.CadastrarTurma(turma);
-            Console.WriteLine("Turma cadastrada com sucesso");
+            Professor professor = new Professor();
+            Console.WriteLine("Digite o nome do professor: ");
+            professor.Nome = Console.ReadLine();
+            Console.WriteLine("Digite a especialidade do professor: ");
+            professor.Especialidade = Console.ReadLine();
+            return professor;
+        }
+
+        public void CadastrarProfessor(Professor professor)
+        {
+            _professores.Add(professor);
+        }
+
+        public Turma CriarTurma()
+        {
+            Turma turma = new Turma();
+            Console.WriteLine("Digite o nome da turma: ");
+            turma.Nome = Console.ReadLine();
+            Console.WriteLine("Digite a data de início da turma (dd/mm/yyyy): ");
+            turma.DataInicio = DateTime.Parse(Console.ReadLine());
+            return turma;
+        }
+
+        public void CadastrarTurma(Turma turma)
+        {
+            _turmas.Add(turma);
         }
     }
 
-    private void ListarAlunos()
+    public class Usuario
     {
-        List<Aluno> alunos = _alunoUC.ListarAlunos();
-        foreach (Aluno aluno in alunos)
+        public string Nome { get; set; }
+        public string Username { get; set; }
+        public string Senha { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString()
         {
-            Console.WriteLine(aluno.ToString());
+            return $"Nome: {Nome}, Username: {Username}, Email: {Email}";
         }
+    }
+
+    public class Aluno
+    {
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome}, Data de Nascimento: {DataNascimento.ToShortDateString()}";
+        }
+    }
+
+    public class Professor
+    {
+        public string Nome { get; set; }
+        public string Especialidade { get; set; }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome}, Especialidade: {Especialidade}";
+        }
+    }
+
+    public class Turma
+    {
+        public string Nome { get; set; }
+        public DateTime DataInicio { get; set; }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome}, Data de Início: {DataInicio.ToShortDateString()}";
+        }
+    }
+
+    public class UsuarioLoginDTO
+    {
+        public string Username { get; set; }
+        public string Senha { get; set; }
     }
 }
